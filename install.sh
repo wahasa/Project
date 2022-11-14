757c7e329dd42fb9a21decf0995cb4c67001a1d9
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-14 09:00:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 clear
 wget -q https://raw.githubusercontent.com/wahasa/Project/main/Installer/Linux -P ../usr/bin/
 wget -q https://raw.githubusercontent.com/wahasa/Project/main/Installer/Install -P ../usr/bin/
+chmod +x ../usr/bin/Linux
+chmod +x ../usr/bin/Install
 echo ""
 echo "Installation successful,."
 echo ""
