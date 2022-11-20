7dc684386dde15d7bc4ac343842ce0e366e81854
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-20 13:32:47 +0700

    Update and rename alpine3.16.2.sh to alpine3.16.3.sh

--- Linux/Alpine/alpine3.16.3.sh
+++ Linux/Alpine/alpine3.16.3.sh
@@ -129,7 +129,7 @@ if [ -d $folder/var ];then
         echo "You can now start Alpine with 'alpine' script next time"
 	echo ""
 	rm -rf ~/.bash_profile" > $folder/root/.bash_profile  
-	rm alpine3.16.2.sh
+	rm alpine3.16.3.sh
 	rm audiofix.sh
 	bash $bin
 else
