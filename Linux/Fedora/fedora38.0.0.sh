d61710e2121a27643ad2793f310efc98483eef3f
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-20 14:40:17 +0700

    Update fedora38.0.0.sh

--- Linux/Fedora/fedora38.0.0.sh
+++ Linux/Fedora/fedora38.0.0.sh
@@ -26,7 +26,7 @@ if [ "$first" != 1 ];then
                 *)
                         echo "unknown architecture"; exit 1 ;;
                 esac
-                wget "https://github.com/fedora-cloud/docker-brew-fedora/blob/4aa98d132a5424bb790601c94a4c573a58ddd4b3/${archurl}/fedora-38-${archurl}.tar.xz?raw=true" -O $tarball
+                wget https://github.com/fedora-cloud/docker-brew-fedora/blob/4aa98d132a5424bb790601c94a4c573a58ddd4b3/${archurl}/fedora-38-${archurl}.tar.xz?raw=true -O $tarball
         fi
         cur=`pwd`
         mkdir -p "$folder"
