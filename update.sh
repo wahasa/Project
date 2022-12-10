2b9c2ad15d10ff58a3dd4e2bd342f8b7f17dcb52
wahasa <69626847+wahasa@users.noreply.github.com>
2022-12-10 09:25:28 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,9 +5,7 @@ wget -q https://raw.githubusercontent.com/wahasa/Project/main/Installer/Linux -P
 wget -q https://raw.githubusercontent.com/wahasa/Project/main/Installer/Install -P ../usr/bin/
 chmod +x ../usr/bin/Linux
 chmod +x ../usr/bin/Install
-echo ""
 echo "Updating list successful,."
-echo ""
 echo ""
 sleep 1
 echo -n ""
