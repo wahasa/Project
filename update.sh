3a6f32e524f54fd426c68eb2c56f7592bd649fc2
wahasa <69626847+wahasa@users.noreply.github.com>
2022-12-10 09:30:03 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ rm ../usr/bin/Install
 rm ../usr/bin/Linux
 wget -q https://raw.githubusercontent.com/wahasa/Project/main/Installer/Linux -P ../usr/bin/
 wget -q https://raw.githubusercontent.com/wahasa/Project/main/Installer/Install -P ../usr/bin/
+wget -q https://raw.githubusercontent.com/wahasa/Project/main/Uninstaller/Uninstall -P ../usr/bin/
 chmod +x ../usr/bin/Linux
 chmod +x ../usr/bin/Install
 echo "Updating list successful,."
