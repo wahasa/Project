970d4a9ab43b48c4fdca86e567b65cf422c8e287
wahasa <69626847+wahasa@users.noreply.github.com>
2022-12-10 09:52:52 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,30 @@ chmod +x ../usr/bin/Install
 echo ""
 echo "Installation successful,."
 echo ""
+sleep 1
+echo -n ""
+echo -n "L"
+sleep 1
+echo -n "o"
+sleep 1
+echo -n "a"
+sleep 1
+echo -n "d"
+sleep 1
+echo -n "i"
+sleep 1
+echo -n "n"
+sleep 1
+echo -n "g"
+sleep 1
+echo -n ","
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 2
 rm ../usr/bin/Install.*
 rm ../usr/bin/Linux.*
+clear
 Linux
 rm install.sh
