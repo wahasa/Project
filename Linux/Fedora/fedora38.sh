5572eb09bea3788ffc62f96673aac0c79787f82c
wahasa <69626847+wahasa@users.noreply.github.com>
2023-02-02 14:00:39 +0700

    Update fedora38.sh

--- Linux/Fedora/fedora38.sh
+++ Linux/Fedora/fedora38.sh
@@ -16,13 +16,13 @@ if [ "$first" != 1 ];then
                 echo "Download Rootfs, this may take a while base on your internet speed."
                 case `dpkg --print-architecture` in
                 aarch64)
-                        archurl="arm64" ;;
+                        archurl="aarch64" ;;
                 #arm)
                 #       archurl="armhf" ;;
                 amd64)
-                        archurl="amd64" ;;
+                        archurl="x86_64" ;;
                 x86_64)
-                        archurl="amd64" ;;
+                        archurl="x86_64" ;;
                 *)
                         echo "unknown architecture"; exit 1 ;;
                 esac
