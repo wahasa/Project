3eebc457b380fd6e29018af905fd2ca4ee307859
wahasa <69626847+wahasa@users.noreply.github.com>
2023-02-16 13:11:27 +0700

    Create photon-amd64.sh

---
+++ Linux/Photon/photon-amd64.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget "https://github.com/vmware/photon-docker-image/blob/2e9a9ec4cc3a40565d67c2c3495e5ef3dd046770/docker/photon-rootfs-5.0-9e778f409.tar.gz?raw=true" -O photon-rootfs.tar.gz
+rm photon-amd64.sh
