16d3ba52de06c9375a7a874ec354929ad236d18b
wahasa <69626847+wahasa@users.noreply.github.com>
2023-02-24 12:40:42 +0700

    Create parrot5.2.sh

---
+++ Linux/Parrot/parrot5.2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+case `dpkg --print-architecture` in
+      aarch64)
+           archurl="arm64" ;;
+      arm)
+           archurl="armhf" ;;
+      amd64)
+           archurl="amd64" ;;
+      x86_64)
+           archurl="amd64" ;;
+      *)
+           echo "unknown architecture"; exit 1 ;;
+      esac
+      wget "https://raw.githubusercontent.com/wahasa/Project/main/Linux/Parrot/parrot-${archurl}.sh"
+chmod +x parrot-${archurl}.sh
+bash parrot-${archurl}.sh
+rm parrot5.2.sh
