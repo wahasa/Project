ad56fdc18a38e69ecf1b98da4f5c9102781f76b4
wahasa <69626847+wahasa@users.noreply.github.com>
2023-02-25 11:05:18 +0700

    Update p3-arm64.sh

--- Linux/Photon/p3-arm64.sh
+++ Linux/Photon/p3-arm64.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget "" -O photon-rootfs.tar.gz
+wget "https://github.com/vmware/photon-docker-image/blob/e83c8b986288cc735e607f5025bdf58f40b5be0e/docker/photon-rootfs-3.0-c7389267e.tar.gz?raw=true" -O photon-rootfs.tar.gz
 rm p3-arm64.sh
