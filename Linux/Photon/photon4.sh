e44900275f300790680a7609dd94ec31003e0186
wahasa <69626847+wahasa@users.noreply.github.com>
2023-02-25 11:10:43 +0700

    Update photon4.sh

--- Linux/Photon/photon4.sh
+++ Linux/Photon/photon4.sh
@@ -26,7 +26,7 @@ if [ "$first" != 1 ];then
                 *)
                         echo "unknown architecture"; exit 1 ;;
                 esac
-                wget https://raw.githubusercontent.com/wahasa/Project/main/Linux/Photon/photon-${archurl}.sh && chmod +x photon-${archurl}.sh && ./photon-${archurl}.sh
+                wget https://raw.githubusercontent.com/wahasa/Project/main/Linux/Photon/p4-${archurl}.sh && chmod +x p4-${archurl}.sh && ./p4-${archurl}.sh
         fi
         cur=`pwd`
         mkdir -p "$folder"
