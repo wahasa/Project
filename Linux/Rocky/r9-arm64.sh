b5f507d6ed9296a6714d46dee7e7812647985032
wahasa <69626847+wahasa@users.noreply.github.com>
2023-02-25 13:33:48 +0700

    Create r9-arm64.sh

---
+++ Linux/Rocky/r9-arm64.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget "https://github.com/rocky-linux/sig-cloud-instance-images/blob/a44de1cb9192969dacd996beb22c152020908532/layer.tar.xz?raw=true" -O rocky-rootfs.tar.xz
+rm r9-arm64.sh
