500f976ee5c423ce393ac8f4dd5028d33ca0b3e3
wahasa <69626847+wahasa@users.noreply.github.com>
2023-02-25 13:32:42 +0700

    Create r9-amd64.sh

---
+++ Linux/Rocky/r9-amd64.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget "https://github.com/rocky-linux/sig-cloud-instance-images/blob/c554918c3fb7432f70802459ee942fead8121cae/layer.tar.xz?raw=true" -O rocky-rootfs.tar.xz
+rm r9-amd64.sh
