bbd556c5fb5b2571ac74d56c35f678a4db19b2f4
wahasa <69626847+wahasa@users.noreply.github.com>
2023-02-25 13:34:23 +0700

    Create r8-arm64.sh

---
+++ Linux/Rocky/r8-arm64.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget "https://github.com/rocky-linux/sig-cloud-instance-images/blob/f509fe83af8a893dff234befbc76458f6343b665/layer.tar.xz?raw=true" -O rocky-rootfs.tar.xz
+rm r8-arm64.sh
