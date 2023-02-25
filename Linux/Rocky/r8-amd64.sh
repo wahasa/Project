cbfa790eefc390fd9b270de2690057dea91a6d4a
wahasa <69626847+wahasa@users.noreply.github.com>
2023-02-25 13:33:12 +0700

    Create r8-amd64.sh

---
+++ Linux/Rocky/r8-amd64.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget "https://github.com/rocky-linux/sig-cloud-instance-images/blob/65d3f79aa8b5d0a9ed1270470e585539f7d59cd1/layer.tar.xz?raw=true" -O rocky-rootfs.tar.xz
+rm r8-amd64.sh
