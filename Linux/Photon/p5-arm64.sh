236265f31f85fe02928207fc7ea8eb4792e1e23e
wahasa <69626847+wahasa@users.noreply.github.com>
2023-02-25 10:52:10 +0700

    Update and rename photon-arm64.sh to p5-arm64.sh

--- Linux/Photon/p5-arm64.sh
+++ Linux/Photon/p5-arm64.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget "https://github.com/vmware/photon-docker-image/blob/59c6fe9afe741f4dd05d38f8060da0fee6c41a26/docker/photon-rootfs-5.0-9e778f409.tar.gz?raw=true" -O photon-rootfs.tar.gz
-rm photon-arm64.sh
+rm p5-arm64.sh
