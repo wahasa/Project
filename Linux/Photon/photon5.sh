b933dab3e032551f63f63079c31729feb35b2365
wahasa <69626847+wahasa@users.noreply.github.com>
2023-02-25 10:45:11 +0700

    Update and rename photon.sh to photon5.sh

--- Linux/Photon/photon5.sh
+++ Linux/Photon/photon5.sh
@@ -101,6 +101,6 @@ echo " "
 echo "You can now start Photon with 'photon' script next time"
 echo " "
 rm -rf ~/.bash_profile" > $folder/root/.bash_profile   
-   #photon.sh
+   photon5.sh
    rm audiofix.sh
 bash $bin
