bd1b9163765e51c671402a116a18271ddfba816d
wahasa <69626847+wahasa@users.noreply.github.com>
2023-02-25 11:01:20 +0700

    Update p4-amd64.sh

--- Linux/Photon/p4-amd64.sh
+++ Linux/Photon/p4-amd64.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget "" -O photon-rootfs.tar.gz
+wget "https://github.com/vmware/photon-docker-image/blob/9a3cf5e9131824b9c386fc633d0f06bfceffef67/docker/photon-rootfs-4.0-dc557b3d5.tar.gz?raw=true" -O photon-rootfs.tar.gz
 rm p4-amd64.sh
